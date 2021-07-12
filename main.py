4a60de2d4543841926fa7b9a71d74a0b7f7f37c6
raked <raked@users.noreply.github.com>
2021-07-12 13:42:01 -0400

    Added Starter (Graphic?)

--- main.py
+++ main.py
@@ -23,7 +23,16 @@ starter3 = {
 def main():
     print("Welcome to Biorage RPG!")
     name = input("Please enter a name for your character: ")
-    print("Choose your starter")
+    print("\t\tChoose your starter")
+    print("\tStarter 1\tStarter 2\tStarter 3")
+    print("Name:\t" + starter1["name"] + "\t" +
+          starter2["name"] + "\t" + starter3["name"])
+    print("Damage:\t" + str(starter1["damage"]) + "\t\t" +
+          str(starter2["damage"]) + "\t\t" + str(starter3["damage"]))
+    print("Health:\t" + str(starter1["health"]) + "\t\t" +
+          str(starter2["health"]) + "\t\t" + str(starter3["health"]))
+    print("Stamina:" + str(starter1["stamina"]) + "\t\t" +
+          str(starter2["stamina"]) + "\t\t" + str(starter3["stamina"]))
     starterChoice = input("Enter your starter choice (1, 2, 3): ")
     if (starterChoice == "1" or starterChoice == "2" or starterChoice == "3"):
         if (starterChoice == "1"):
