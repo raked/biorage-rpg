dfb6e9ade81af115627fa574d54d1cbf6ac950b1
raked <raked@users.noreply.github.com>
2021-07-16 16:27:36 -0400

    Added catchphrases, random choice, images

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # encoding: utf8
 import time
 import sys
+import random
 printf = sys.stdout.write
 
 starter1 = {
@@ -27,6 +28,9 @@ starter3 = {
 enemy1 = {
     "name": "Radioactive Waste",
     "image": "     _.-^^---....,,--       \n _--                  --_  \n<                        >)\n|                         | \n \._                   _./  \n    ```--. . , ; .--'''       \n          | |   |             \n       .-=||  | |=-.   \n       `-=#$%&%$#=-'   \n          | ;  :|     \n _____.,-#%&$@%#&#~,._____",
+    "catchphrase1": "Don't stand too close!",
+    "catchphrase2": "Make sure to be equipped with proper PPE!",
+    "catchphrase3": "Why are you itchy?",
     "damage": 3,
     "health": 1,
     "stamina": 2
@@ -35,6 +39,9 @@ enemy1 = {
 enemy2 = {
     "name": "The Troll",
     "image": "░░░░░▄▄▄▄▀▀▀▀▀▀▀▀▄▄▄▄▄▄░░░░░░░\n░░░░░█░░░░▒▒▒▒▒▒▒▒▒▒▒▒░░▀▀▄░░░░\n░░░░█░░░▒▒▒▒▒▒░░░░░░░░▒▒▒░░█░░░\n░░░█░░░░░░▄██▀▄▄░░░░░▄▄▄░░░░█░░\n░▄▀▒▄▄▄▒░█▀▀▀▀▄▄█░░░██▄▄█░░░░█░\n█░▒█▒▄░▀▄▄▄▀░░░░░░░░█░░░▒▒▒▒▒░█\n█░▒█░█▀▄▄░░░░░█▀░░░░▀▄░░▄▀▀▀▄▒█\n░█░▀▄░█▄░█▀▄▄░▀░▀▀░▄▄▀░░░░█░░█░\n░░█░░░▀▄▀█▄▄░█▀▀▀▄▄▄▄▀▀█▀██░█░░\n░░░█░░░░██░░▀█▄▄▄█▄▄█▄████░█░░░\n░░░░█░░░░▀▀▄░█░░░█░█▀██████░█░░\n░░░░░▀▄░░░░░▀▀▄▄▄█▄█▄█▄█▄▀░░█░░\n░░░░░░░▀▄▄░▒▒▒▒░░░░░░░░░░▒░░░█░\n░░░░░░░░░░▀▀▄▄░▒▒▒▒▒▒▒▒▒▒░░░░█░\n░░░░░░░░░░░░░░▀▄▄▄▄▄░░░░░░░░█░░",
+    "catchphrase1": "Step 1: Oil floats on water.",
+    "catchphrase2": "problem?",
+    "catchphrase3": ":D",
     "damage": 1,
     "health": 3,
     "stamina": 2
@@ -42,7 +49,10 @@ enemy2 = {
 
 enemy3 = {
     "name": "marco from BorderLands 3",
-    "image": "",
+    "image": "#############\n##         ##\n#  ~~   ~~  #\n#  ()   ()  #\n(     ^     )\n |         | \n |  {===}  | \n  \       /  \n /  -----  \ ",
+    "catchphrase1": "Welcome to my shop!",
+    "catchphrase2": "A day without slaughter is like a day without sunshine.",
+    "catchphrase3": "No no no, slag him first!",
     "damage": 2,
     "health": 2,
     "stamina": 1
@@ -58,7 +68,8 @@ def printText(text, speed=.05):  # text defined as variable, speed in seconds pe
 
 
 def fightSequence(enemy):
-    printText(enemy["image"], .005)
+    printText(enemy["image"] + " " + random.choice([enemy["catchphrase1"],
+              enemy["catchphrase2"], enemy["catchphrase3"]]), .005)
     printText("A random '" + enemy["name"] + "' has appeared!")
     printText("It doesn't look very friendly.")
 
@@ -78,13 +89,13 @@ def main():
     printText("\t\tChoose your starter", .01)
     printText("\tStarter 1\tStarter 2\tStarter 3", .01)
     printText("Name:\t" + starter1["name"] + "\t" +
-              starter2["name"] + "\t" + starter3["name"])
+              starter2["name"] + "\t" + starter3["name"], .01)
     printText("Damage:\t" + str(starter1["damage"]) + "\t\t" +
-              str(starter2["damage"]) + "\t\t" + str(starter3["damage"]))
+              str(starter2["damage"]) + "\t\t" + str(starter3["damage"]), .01)
     printText("Health:\t" + str(starter1["health"]) + "\t\t" +
-              str(starter2["health"]) + "\t\t" + str(starter3["health"]))
+              str(starter2["health"]) + "\t\t" + str(starter3["health"]), .01)
     printText("Stamina:" + str(starter1["stamina"]) + "\t\t" +
-              str(starter2["stamina"]) + "\t\t" + str(starter3["stamina"]))
+              str(starter2["stamina"]) + "\t\t" + str(starter3["stamina"]), .01)
     printText("----------------------------------------------------", .01)
     starterChoice = printText("Enter your starter choice (1, 2, 3) below", .01)
     starterChoice = input()
@@ -105,7 +116,7 @@ def main():
     print("Name:", playerinfo["name"], "\n" +
           "Starter:", playerinfo["starter"]["name"])
 
-    fightSequence(enemy1)
+    fightSequence(random.choice([enemy1, enemy2, enemy3]))
 
 
 main()
